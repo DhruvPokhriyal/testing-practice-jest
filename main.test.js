bb350a2c5ddb4db3e4b4fd82b7c67bed2b5e3f1f
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-02 14:35:22 +0530

    Create test case for reverseString function

--- main.test.js
+++ main.test.js
@@ -1,12 +1,28 @@
-import { capitalize } from "./main";
+import { capitalize, reverseString } from "./main";
 
-describe;
-test("Capitalize the given string", () => {
-    const testCases = [
-        { input: "hello World", output: "Hello World" },
-        { input: "dummy StrIng", output: "Dummy String" },
-    ];
-    for (const testCase of testCases) {
-        expect(capitalize(testCase.input)).toBe(testCase.output);
-    }
+describe("capitalize function", () => {
+    test("Capitalize the given string", () => {
+        const testCases = [
+            { input: "hello World", output: "Hello World" },
+            { input: "dummy StrIng", output: "Dummy String" },
+        ];
+        for (const testCase of testCases) {
+            expect(capitalize(testCase.input)).toBe(testCase.output);
+        }
+    });
+    test("Capitalize single character", () => {
+        expect(capitalize("m")).toBe("M");
+    });
+});
+
+describe("reverseString function", () => {
+    test("reverse simple word", () => {
+        const testCases = [
+            { input: "uwu", output: "uwu" },
+            { input: "dummy", output: "ymmud" },
+        ];
+        for (const testCase of testCases) {
+            expect(reverseString(testCase.input)).toBe(testCase.output);
+        }
+    });
 });
